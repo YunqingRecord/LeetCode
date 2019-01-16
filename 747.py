f0dde5625f73d215263c57a2e325e41c243211e3
Yunqing <yunqingqueue@gmail.com>
2019-01-16 15:19:13 +0800

    Merge branch 'master' of github.com:YunqingRecord/LeetCode

---
+++ 747.py
@@ -0,0 +1,36 @@
+class Solution:
+    def dominantIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        m = max(nums)
+        loc = nums.index(m)
+        for i in nums:
+            if m == i:
+                pass
+            elif m < 2 * i:
+                return -1
+        return loc
+
+
+def dominantIndex(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    m = max(nums)
+    loc = nums.index(m)
+    for i in nums:
+        if m == i:
+            pass
+        elif m < 2*i:
+            return -1
+    return loc
+
+
+nums = [0,0,0,1]
+
+ans = dominantIndex(nums)
+
+

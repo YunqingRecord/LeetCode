10bb8d45643324d82fe6c1b1d8a8d397da6df110
Yunqing <yunqingqueue@gmail.com>
2019-01-16 15:21:50 +0800

    Merge branch 'master' of github.com:YunqingRecord/LeetCode

--- 747.py
+++ 747.py
@@ -14,23 +14,4 @@ class Solution:
         return loc
 
 
-def dominantIndex(nums):
-    """
-    :type nums: List[int]
-    :rtype: int
-    """
-    m = max(nums)
-    loc = nums.index(m)
-    for i in nums:
-        if m == i:
-            pass
-        elif m < 2*i:
-            return -1
-    return loc
-
-
-nums = [0,0,0,1]
-
-ans = dominantIndex(nums)
-
 

89d6207525b8fa0c0a2bab4b624f3f37312ae69b
Yunqing <yunqingqueue@gmail.com>
2019-03-24 12:36:26 +0800

    The Sort Algorithms

--- InsertionSort.py
+++ InsertionSort.py
@@ -5,24 +5,20 @@
 def InsertionSort(ls):
     '''
 
-    :param ls: the list to be sorted
-    :return:  the sorted list by insertion algo.
+    :param ls:  the list to be sorted
+    :return:  the sorted list
     '''
     n = len(ls)
-    for i in range(1, n):
+    for i in range(1, n):  # 为什么是 1开始，因为从已经排好的序列末尾开始向前比较。后面会出现i-1
         j = i
-        target = ls[i]  # The value to be inserted
-        while j > 0 and target > ls[j-1]:
-            ls[j] = ls[j-1]
-            j = j-1
-            ls[j] = target
+        target = ls[i]  # 每次要插入的数值， 需要额外空间的地方
+        while j > 0 and target > ls[j-1]:  # 对已经排好的序列，从后往前逐渐比较
+            ls[j] = ls[j-1]  # 如果已经排序的最后一位比目标值更小，那么让位出来，重复此过程
+            j = j-1  # 位次往前轮, 如果下次也这样，那么继续腾位置，改变的只有一个值。
+        ls[j] = target
     return ls
 
 
-m = InsertionSort([1, 2, 3, 4])
+m = InsertionSort([1, 3, 2, 4, 5, 9, 4])
 
 print(m)
-
-
-
-
